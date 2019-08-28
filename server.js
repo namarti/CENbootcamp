d4b8b91448145456182ceff65ce6aceccc094d9b
Nathan Martin <na.martin@ufl.edu>
2019-08-28 12:54:53 -0400

    camp1 commit

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
+	
+
+	if (parsedUrl.pathname === '/listings' && request.method === 'GET' ){
+		response.writeHead(200,{ 'Content-Type' : 'application/json'} );
+		response.end(listingData);
+	}else{
+		response.writeHead(404,null);
+		response.end('Bad gateway error');
+	}
 
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
@@ -27,25 +36,33 @@ var requestHandler = function(request, response) {
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-
-    HINT: Check out this resource on fs.readFile
+  
+	/*This callback function should save the data in the listingData variable,*/
+	//then start the server. 
+    /*HINT: Check out this resource on fs.readFile
     //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
     HINT: Read up on JSON parsing Node.js
    */
 
     //Check for errors
+	if (err) {
+		throw err;
+	}
   
 
    //Save the sate in the listingData variable already defined
+	listingData = data;
   
 
   //Creates the server
+	var server = http.createServer(requestHandler);
   
   //Start the server
+	server.listen(port, function(){
+		console.log('Server listening on: http://127.0.0.1:' + port);
+	})
+
 
 
 });
